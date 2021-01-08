92a4dcafa41dd2ec4369b266e0d7c583dc4990c7
Dima Danilov <dima.danilov.2012@mail.ru>
2021-01-08 16:08:46 +0300

    added export database on html page

--- productssite/main/views.py
+++ productssite/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Products
 
 def index(request):
-    return render(request, 'main/index.html')
+    products = Products.objects.all()
+    return render(request, 'main/index.html', {'products': products})
